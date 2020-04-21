d2ba3fb0cd39c161f8fde314e065f562a973afd5
dylan@dylanhulbert.com <dylan@dylanhulbert.com>
2020-04-21 01:38:55 -0700

    db updates

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
-
 let connection;
+
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
@@ -12,7 +12,6 @@ connection = mysql.createConnection({
   database: "burgers_db"
 });
 };
-
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
